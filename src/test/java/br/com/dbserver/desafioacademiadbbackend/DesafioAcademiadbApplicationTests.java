a07c4be4f84166919f16f70707fda7b7aa984d2b
vitorsedgar <vitorsedgar@gmail.com>
2020-10-08 16:10:55 -0300

    adicionando profiles

--- src/test/java/br/com/dbserver/desafioacademiadbbackend/DesafioAcademiadbApplicationTests.java
+++ src/test/java/br/com/dbserver/desafioacademiadbbackend/DesafioAcademiadbApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.dbserver.desafioacademiadbbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "test")
 class DesafioAcademiadbApplicationTests {
 
     @Test
