eda50bdbe08c2a916a87410ca0c43473a8f13492
vitorsedgar <vitorsedgar@gmail.com>
2020-10-09 10:43:15 -0300

    Criacao de tabelas medico unidade e atendimento

---
+++ src/main/java/br/com/dbserver/desafioacademiadbbackend/repository/MedicoRepository.java
@@ -0,0 +1,7 @@
+package br.com.dbserver.desafioacademiadbbackend.repository;
+
+import br.com.dbserver.desafioacademiadbbackend.entity.MedicoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//public interface MedicoRepository extends JpaRepository<MedicoEntity, Long> {
+//}
